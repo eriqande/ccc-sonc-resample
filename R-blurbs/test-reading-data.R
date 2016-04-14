89c9d3ba383a311ef3e35b24d875ee3ed9ffc85c
Eric C. Anderson <eric.anderson@noaa.gov>
2016-04-14 14:30:05 -0700

    Delete final "empty" columns in excel data files.
    there were some columns with entries (probably whitespace) that made it impossible to read with readxl.

--- R-blurbs/test-reading-data.R
+++ R-blurbs/test-reading-data.R
@@ -7,9 +7,17 @@ library(readxl)
 library(dplyr)
 
 
+# note that column names (like for loci) are not unique, so be careful there.
+
 pops <- read_excel("data/coho_coastw_pops_TABLE.xlsx", sheet = 1) %>%
   tbl_df
 
 
-geno_repo <- read_excel("data/coho_coastw_2003_2015_genotypes.xlsx", sheet = 1, col_names = FALSE) %>%
+geno_repo <- read_excel("data/coho_coastw_2003_2015_genotypes.xlsx", sheet = 1, col_names = TRUE) %>%
   tbl_df
+
+geno_infile <- read_excel("data/coho_coastw_2003_2015_genotypes.xlsx", sheet = 2, col_names = TRUE) %>%
+  tbl_df 
+
+geno_discards <- read_excel("data/coho_coastw_2003_2015_genotypes.xlsx", sheet = 3, col_names = TRUE) %>%
+  tbl_df 
